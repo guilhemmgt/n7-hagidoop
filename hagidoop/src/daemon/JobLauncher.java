ee93de0b347721ae29176f31035d3467f5073c1a
Achille Jeanvoine <jeanvoine.achille@gmail.com>
2024-01-15 19:11:09 +0100

    modif joblauncher threads

--- hagidoop/src/daemon/JobLauncher.java
+++ hagidoop/src/daemon/JobLauncher.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.rmi.Naming;
+import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,7 +19,10 @@ import io.NetworkReaderWriterImpl;
 
 public class JobLauncher {
 
-	public static void startJob (MapReduce mr, int format, String fname) {
+	public static NetworkReaderWriterImpl nrwMain;
+	public static List<Thread> threads;
+
+	public static void startJob(MapReduce mr, int format, String fname) {
 		// Récupère les noeuds via le fichier config
 		List<KV> nodes = new ArrayList<KV>();
 		try {
@@ -34,34 +38,52 @@ public class JobLauncher {
 
 		// Initialise le NetworkReaderWriter
 		InetAddress addr;
-		NetworkReaderWriter nrw = null;
-    	try {
-			addr = InetAddress.getLocalHost(); 
-			String hostName = addr.getHostName().split(".")[0]; // addr.getHostName() renvoie vador.enseeiht.fr, on ne souhaite récupérer que vador
-			nrw = new NetworkReaderWriterImpl(4000, hostName);
-			nrw.openServer();
+		try {
+			addr = InetAddress.getLocalHost();
+			String hostName = addr.getHostName().split(".")[0]; // addr.getHostName() renvoie vador.enseeiht.fr, on ne
+																// souhaite récupérer que vador
+			nrwMain = new NetworkReaderWriterImpl(4000, hostName);
+			nrwMain.openServer();
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
-		
 
 		// Lance les map
 		for (KV node : nodes) {
 			try {
 				Worker s = (Worker) Naming.lookup("//" + node.k + ":" + node.v + "/worker");
-				s.runMap(mr, frw, nrw);
+
+				Thread t1 = new Thread(new Runnable() {
+					@Override
+					public void run() {
+						try {
+							s.runMap(mr, frw, nrwMain);
+						} catch (RemoteException e) {
+							e.printStackTrace();
+						}
+					}
+				});
+				threads.add(t1);
+				t1.start();
+
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 		}
 
-		// Accepte tous les maps
-		for (int i = 0; i < nodes.size(); i++) {
-			nrw.accept();
-			System.out.println("Accept");
+		// Attendre que tous les maps soient terminés
+		for(Thread thr : threads){
+			try {
+				thr.join();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
 		}
 
+		// récupérer les KV envoyés au reduce
+		System.out.println(NetworkReaderWriterImpl.sharedQueue.toArray());
+
 		// Ferme le NetworkReaderWriter
-		nrw.closeServer();
+		nrwMain.closeServer();
 	}
 }
